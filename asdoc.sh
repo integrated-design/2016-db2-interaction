91b4b0f368af9c7e1a0963e6a8fc6b317060c993
Yukiya Okuda <yukiya@alumican.net>
2016-10-30 18:17:15 +0900

    ASDocの場所を変更

---
+++ asdoc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd "/Users/yukiya/Dropbox/Private/work/shouwakiden/300_プロジェクト/多摩美術大学 - 統合デザイン学科/01_Subject/2016_DB2_Interaction/github"
+rm -rf docs
+mkdir docs
+asdoc -doc-sources dev/src -output docs
--- dev/asdoc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd "/Users/yukiya/Dropbox/Private/work/shouwakiden/300_プロジェクト/多摩美術大学 - 統合デザイン学科/01_Subject/2016_DB2_Interaction/github/dev"
-rm -rf doc
-mkdir doc
-asdoc -doc-sources src -output doc
